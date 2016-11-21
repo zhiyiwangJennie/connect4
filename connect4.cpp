2ef5531baf73ea1d2838ac3cd6b30cfa6a974e2e
Isabelle Kong <isabelle@Isabelles-MacBook-Pro.local>
2016-11-21 18:38:39 -0500

    11/21/2016 (6.38pm)

--- connect4.cpp
+++ connect4.cpp
@@ -5,8 +5,7 @@
 #include <string>
 #include <cctype>
 using namespace std;
-//hello
-//command commit1
+
 int main(int argc, char *argv[]) {
     cout << "Press 1 for terminal and 2 for graphics: ";
     int choice;
@@ -26,23 +25,115 @@ int main(int argc, char *argv[]) {
 }
 
 void terminalPlay(){
-    cout << "\nEnter: the column to move, q to quit, s to save" << endl;
-    cout <<  " enter your move: ";
-    cout << "Thanks for playing!"<< endl;
-    cout << "ILLEGAL MOVE: Try again" << endl;
-    cout << "Congratulations ";
-    cout << "! You won!" << endl;
-    cout << "Draw!" << endl;
+    
+    //name of player 1
+    string p1;
+    //name of player 2
+    string p2;
+    int userColumn;
+    char userInput;
+    Board boardChoice;
+    Result moveResult = NoResult;
+    
+    boardChoice = getBoard(p1, p2);
+    while ((userInput != 'q') || (moveResult != Win) || (moveResult != Draw)) {
+        boardChoice.prettyPrintBoard(cout);
+        cout << "\nEnter: the column to move, q to quit, s to save" << endl;
+        //player 1's turn
+        if (boardChoice.toMove() == 1) {
+            cout <<  p1 << " enter your move: ";
+        }
+        //player 2's turn
+        else if (boardChoice.toMove() == 2) {
+            cout <<  p2 << " enter your move: ";
+        }
+        cin >> userColumn;
+        //if user entered a character instead of a column number
+        if (cin.fail()) {
+            cin.clear();
+            cin >> userInput;
+            userInput = tolower(userInput);
+            if (userInput == 's') {
+                saveBoard(p1, p2, boardChoice);
+            }
+            else if (userInput == 'q') {
+                break;
+            }
+            else {
+                cout << "ILLEGAL MOVE: Try again" << endl;
+            }
+        }
+        //if user entered a column number
+        else {
+            //make move if column input is valid (from col1 to col7)
+            if ((userColumn >= 1) && (userColumn <= 7)) {
+                moveResult = boardChoice.makeMove(userColumn);
+            }
+            else {
+                cout << "ILLEGAL MOVE: Try again" << endl;
+            }
+        }
+    }
+    //end of the game
+    if (userInput == 'q') {
+        cout << "Thanks for playing!"<< endl;
+    }
+    else if (moveResult == Win) {
+        //player 2 wins if board is won before player 1's turn
+        if (boardChoice.toMove() == 1) {
+            cout << "Congratulations " << p2;
+            cout << "! You won!" << endl;
+        }
+        //player 1 wins if board is won before player 2's turn
+        else if (boardChoice.toMove() == 2) {
+            cout << "Congratulations " << p1;
+            cout << "! You won!" << endl;
+        }
+        boardChoice.prettyPrintBoard(cout);
+    }
+    else if (moveResult == Draw) {
+        cout << "Draw!" << endl;
+        boardChoice.prettyPrintBoard(cout);
+    }
 }
 
 Board getBoard(string &p1, string &p2) {
+    char userChoice;
+    string fileName;
+    ifstream inFS;
+    
     cout << "Enter L to load a game, or P to play: " << flush;
-    cout << "Player 1, enter your name: " << flush;
-    cout << endl << "Player 2, enter your name: " << flush;
-    cout << "Enter the filename: " << flush;
+    cin >> userChoice;
+    userChoice = tolower(userChoice);
+    if (userChoice == 'p') {
+        cout << "Player 1, enter your name: " << flush;
+        cin >> p1;
+        cout << endl << "Player 2, enter your name: " << flush;
+        cin >> p2;
+        Board newBoard;
+    }
+    //all other user input except "play" loads saved file
+    else {
+        cout << "Enter the filename: " << flush;
+        cin >> fileName;
+        inFS.open(fileName);
+        inFS >> p1;
+        inFS >> p2;
+        //open saved file with FEN string info(TODO)
+        inFS.close();
+    }
     return Board();
 }
 
 void saveBoard(string p1, string p2, const Board &board) {
+    ofstream outFile;
+    string userFile;
+    
     cout << "Enter the filename: " << flush;
+    cin >> userFile;
+    outFile.open(userFile);
+    outFile << p1 << endl;
+    outFile << p2 << endl;
+    //TODO: print out FEN string of saved board to file
+    outFile.close();
 }
